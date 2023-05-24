3f136323843cb9b24212ba61da47cde48b37efaf
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-05-24 15:20:19 -0300

    .

--- docker.sh
+++ docker.sh
@@ -21,53 +21,54 @@ if ! command -v docker &>/dev/null; then
 else
 	echo "Docker ya está instalado."
 
-	# Obtener la versión actual de Docker
-	current_version=$(docker --version | awk '{print $3}')
-	echo "Versión actual de Docker: $current_version"
-
-	# Obtener la última versión estable de Docker
-	latest_version=$(curl -sSL https://download.docker.com/linux/static/stable/x86_64/ | grep -oE 'docker-[0-9]+\.[0-9]+\.[0-9]+.tgz' | grep -oE '[0-9]+\.[0-9]+\.[0-9]+' | sort -V | tail -n 1)
-	echo "Última versión estable de Docker: $latest_version"
-
-	# Verificar si se requiere actualizar Docker
-	if [[ $current_version != $latest_version ]]; then
-		echo "Actualizando Docker a la versión $latest_version..."
-		curl -fsSL https://get.docker.com | sh
-		sudo usermod -aG docker "$USER"
-		echo "Docker se ha actualizado correctamente a la versión $latest_version."
+	# Verificar si hay actualizaciones disponibles para Docker
+	if sudo apt-get update -qq --allow-releaseinfo-change && sudo apt-get upgrade -s docker-ce | grep -q 'Inst docker-ce'; then
+		echo "Hay actualizaciones disponibles para Docker. Actualizando Docker..."
+		sudo apt-get update -qq --allow-releaseinfo-change && sudo apt-get upgrade -y docker-ce
+		echo "Docker se ha actualizado correctamente."
 	else
-		echo "Docker ya está en la última versión estable."
+		echo "Docker ya está en la última versión."
 	fi
+fi
 
+# Verificar si pip está instalado
+if ! command -v pip &>/dev/null; then
+	echo "pip no está instalado. Instalando pip..."
+	sudo apt-get install -y python3-pip
+	echo "pip se ha instalado correctamente."
+else
+	echo "pip ya está instalado."
+fi
+
+# Verificar si pip está en su última versión
+installed_version=$(pip --version | awk '{print $2}')
+latest_version=$(pip install --upgrade pip >/dev/null && pip --version | awk '{print $2}')
+if [[ $installed_version != $latest_version ]]; then
+	echo "Hay una actualización disponible para pip. Actualizando pip..."
+	pip install --upgrade pip
+	echo "pip se ha actualizado correctamente."
+else
+	echo "pip ya está en la última versión."
 fi
 
 # Verificar si Docker Compose está instalado
 if ! command -v docker-compose &>/dev/null; then
 	echo "Docker Compose no está instalado. Instalando Docker Compose..."
-	sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
-	sudo chmod +x /usr/bin/docker-compose
+	sudo pip install docker-compose
 	echo "Docker Compose se ha instalado correctamente."
 else
 	echo "Docker Compose ya está instalado."
 
-	# Obtener la versión actual de Docker Compose
-	current_version=$(docker-compose --version | awk '{print $3}')
-	echo "Versión actual de Docker Compose: $current_version"
-
-	# Obtener la última versión estable de Docker Compose
+	# Verificar si hay actualizaciones disponibles para Docker Compose
+	installed_version=$(docker-compose --version | awk '{print $3}')
 	latest_version=$(curl -sSL https://api.github.com/repos/docker/compose/releases/latest | grep -oE '([0-9]+\.){2}[0-9]+' | head -n 1)
-	echo "Última versión estable de Docker Compose: $latest_version"
-
-	# Verificar si se requiere actualizar Docker Compose
-	if [[ $current_version != $latest_version ]]; then
-		echo "Actualizando Docker Compose a la versión $latest_version..."
-		sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
-		sudo chmod +x /usr/bin/docker-compose
-		echo "Docker Compose se ha actualizado correctamente a la versión $latest_version."
+	if [[ $installed_version != $latest_version ]]; then
+		echo "Hay una actualización disponible para Docker Compose. Actualizando Docker Compose..."
+		sudo pip install --upgrade docker-compose
+		echo "Docker Compose se ha actualizado correctamente."
 	else
-		echo "Docker Compose ya está en la última versión estable."
+		echo "Docker Compose ya está en la última versión."
 	fi
-
 fi
 
 # Verificar las versiones instaladas
