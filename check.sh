146b388779425ae0c614362fdf9df4767d0f50c6
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-05-24 14:22:26 -0300

    .

--- check.sh
+++ check.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-echo "Hola lucas desde el repo!!!"
+# Verificar si Volta está instalado
+if ! command -v volta &>/dev/null; then
+    echo "Volta no está instalado. Instalando..."
+    curl https://get.volta.sh | bash
+    export PATH="$HOME/.volta/bin:$PATH"
+else
+    echo "Volta está instalado."
+fi
+
+# Verificar la versión de Node.js
+if command -v volta &>/dev/null; then
+    volta --version
+    volta install node
+    node --version
+else
+    echo "No se puede encontrar Volta. No se puede instalar Node.js."
+fi
+
