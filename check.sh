3edd0ae4c4b8eccc88b1c35329932309ff8a6fb9
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-05-24 14:25:26 -0300

    .

--- check.sh
+++ check.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+
+# Verificar si el sistema es Linux
+if [[ "$(uname)" != "Linux" ]]; then
+    echo "Este script solo es compatible con sistemas Linux."
+    exit 1
+fi
+
+# Verificar si el sistema es Ubuntu
+if [[ "$(lsb_release -si)" != "Ubuntu" ]]; then
+    echo "Este script solo es compatible con sistemas Ubuntu."
+    exit 1
+fi
+
+# Tu código aquí para Linux y Ubuntu
+echo "El sistema es Linux y es Ubuntu."
+
+
+
+
+
 # Verificar si Volta está instalado
 if ! command -v volta &>/dev/null; then
     echo "Volta no está instalado. Instalando..."
